a01fdb35c184578c034028f3de5e8a2ca587f1f6
Jakub0821 <jakubmigda120@gmail.com>
2024-01-24 23:31:32 +0100

    Change components 02-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Section from 'components/Section/Section';
+import Component  from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: props.initialGood,
+      neutral: props.initialNeutral,
+      bad: props.initialBad,
+    };
+  }
+
+  static defaultProps = {
+    step: 1,
+    initialGood: 0,
+    initialNeutral: 0,
+    initialBad: 0,
+  };
+
+  static propTypes = {
+    step: PropTypes.number,
+    initialGood: PropTypes.number,
+    initialNeutral: PropTypes.number,
+    initialBad: PropTypes.number,
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback" state={this.state}/>
+      </>
+    );
+  }
+}
+
+export default App;
